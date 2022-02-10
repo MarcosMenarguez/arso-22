36340aceba44247ccb56e7c88bb218bccfaf87d7
MarcosMenarguez <marcos.menarguez@gmail.com>
2022-02-10 10:41:40 +0100

    Soluciones Semana 2

---
+++ soluciones/src/main/java/dom/ejercicio1/Main.java
@@ -0,0 +1,50 @@
+﻿package dom.ejercicio1;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class Main {
+
+	// Para no complicar el código del ejercicio, no tratamos las excepciones
+	
+	public static void main(String[] args) throws Exception {
+
+		final String documento = "xml/acta.xml";
+		
+		// Construye un analizador DOM
+		
+		DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factoria.newDocumentBuilder();
+
+		Document doc = builder.parse(documento);
+
+		double acumulado = 0; 
+		int notas = 0;
+		
+				
+		NodeList nodos = doc.getElementsByTagName("nota");
+		for (int i = 0; i < nodos.getLength(); i++) {
+			
+			Element nota = (Element) nodos.item(i);
+			
+			// Comprueba si es una calificación
+			
+			if (nota.getParentNode().getNodeName().equals("calificacion")) {
+				acumulado += Double.parseDouble(nota.getTextContent());
+				notas++;
+			}
+		}
+		
+		if (notas > 0)
+			System.out.println("Nota media DOM: " + acumulado / notas);
+		else
+			System.out.println("El documento no contiene notas");
+
+		System.out.println("Fin.");
+	}
+
+}
---
+++ soluciones/src/main/java/dom/ejercicio2/Main.java
@@ -0,0 +1,60 @@
+﻿package dom.ejercicio2;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class Main {
+
+	// Para no complicar el código del ejercicio, no tratamos las excepciones
+	
+	public static void main(String[] args) throws Exception {
+
+		final String documento = "xml/acta.xml";
+		final String salida = "xml/acta-modificado.xml";
+		
+		// Construye un analizador DOM
+		
+		DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factoria.newDocumentBuilder();
+
+		Document doc = builder.parse(documento);
+		
+		// Actualizamos las notas
+		
+		NodeList nodos = doc.getElementsByTagName("nota");
+		
+		for (int i = 0; i < nodos.getLength(); i++) {
+			
+			Element nota = (Element) nodos.item(i);
+			
+			// Actualizamos la nota
+			
+			double valor = Double.parseDouble(nota.getTextContent());
+			valor = Math.min(10, valor + 0.5);
+			
+			nota.setTextContent(Double.toString(valor));			
+		}
+
+		
+		// Almacenamos el árbol DOM en un nuevo documento
+		
+		TransformerFactory tFactoria = TransformerFactory.newInstance();
+		Transformer transformacion = tFactoria.newTransformer();
+		
+		Source input = new DOMSource(doc);
+		Result output = new StreamResult(salida);
+		transformacion.transform(input, output);
+
+		System.out.println("Fin.");
+	}
+}
