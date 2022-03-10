b048920d9961aa2794c7de9f733af61a4ce888b7
MarcosMenarguez <marcos.menarguez@gmail.com>
2022-03-10 09:47:02 +0100

    Semana 7: Controlador REST Bookle

---
+++ bookle-rest/src/main/java/bookle/repositorio/RepositorioActividades.java
@@ -0,0 +1,16 @@
+package bookle.repositorio;
+
+import es.um.bookle.Actividad;
+import repositorio.Repositorio;
+
+public interface RepositorioActividades extends Repositorio<Actividad, String> {
+
+	/*
+	 * El repositorio declara los métodos básicos de gestión de la entidad.
+	 * 
+	 * En esta interfaz podemos añadir métodos más específicos.
+	 */
+	
+	
+	
+}
